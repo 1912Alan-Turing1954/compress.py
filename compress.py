40626f4878666889e59d4802902fa7deef4e151f
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-04-18 17:35:38 -0700

    Fixes & cleaning

--- compress.py
+++ compress.py
@@ -2,54 +2,54 @@ import zlib, sys, time, base64, os
 
 option = str(input('Enter if you would to compress a file or dcompress a file, (Compress/Decompress): '))
 
-# hint = str(input('Enter a hint of where this file may be located (opiontional): '))
+drive = str(input('Enter the drive the file is located in (C/E/D), (Optional): '))
 
-# if hint:
-#     root = f'C:\{hint}'
-# else:
-root = 'C:\\'
+root = ''
 
+if drive == '':
+    root = 'C:\\'
+else:
+    root = drive[0]
 
 def compress():
-
+    start_time = time.time()
     filename = str(input('Enter filename that you wish to compress: '))
 
     for relPath, dirs, files in os.walk(root):
         if filename in files:
             fullPath = os.path.join(root, relPath, filename)
             text = open(fullPath, 'rb').read()
-            path = os.path.split(fullPath)[0]
-            name = os.path.splitext(filename)[0]
-            extension = os.path.splitext(filename)[1]
-            print(f'\nRaw size: {sys.getsizeof(text)}')
             print('---------------')
-            compressed = base64.b64encode(zlib.compress(text,9))
-            print(f'9 compressed size: {sys.getsizeof(compressed)}')
+            print(f'Raw size: {sys.getsizeof(text)}')
             print('---------------')
-            print(f'Compress percentage: {100.0 - ((float(sys.getsizeof(compressed))/float(sys.getsizeof(text))) * 100.0)}%')
+            compressed = base64.b64encode(zlib.compress(text, 9))
+            print(f'Compressed size: {sys.getsizeof(compressed)}')
             print('---------------')
-            
+            print(f'Compress percentage: {round(100.0 - ((float(sys.getsizeof(compressed))/float(sys.getsizeof(text))) * 100.0), 2)}%')
+            print('---------------')
+            print(f'Fullpath to compressed file: {fullPath}')
+            print(f'(Tinme to execute and compress the file: {time.time() - start_time})')
             savecomp = open(fullPath, 'wb').write(compressed)
             return savecomp
 
 def decompress():
+    start_time = time.time()
     filename = str(input('Enter filename that you wish to decompress: '))
-
+    
     for relPath, dirs, files in os.walk(root):
         if filename in files:
             fullPath = os.path.join(root, relPath, filename)
             text = open(fullPath, 'rb').read()
-            path = os.path.split(fullPath)[0]
-            name = os.path.splitext(filename)[0]
-            extension = os.path.splitext(filename)[1]
-            print(f'\nCompressed size: {sys.getsizeof(text)}')
+            print('---------------')
+            print(f'Compressed size: {sys.getsizeof(text)}')
             print('---------------')
             decompressed = zlib.decompress(base64.b64decode(text))
             print(f'Raw size: {sys.getsizeof(decompressed)}')
             print('---------------')
-            print(f'Decompress percentage: {100.0 - (float(sys.getsizeof(text))/float(sys.getsizeof(decompressed)) * 100.0)}%')
+            print(f'Decompress percentage: {round(100.0 - (float(sys.getsizeof(text))/float(sys.getsizeof(decompressed)) * 100.0), 2)}%')
             print('---------------')
-            
+            print(f'Fullpath to compressed file: {fullPath}')
+            print(f'(Tinme to execute and compress the file: {time.time() - start_time})')
             savecomp = open(fullPath, 'wb').write(decompressed)
             return savecomp
 
@@ -57,3 +57,6 @@ if option == 'compress' or option == 'Compress' or option == 'COMPRESS' or optio
     compress()
 elif option == 'decompress' or option == 'Decompress' or option == 'DECOMPRESS' or option == 'd' or option == 'D':
     decompress()
+else:
+    print('Invalid option\n')
+    option = str(input('Enter if you would to compress a file or dcompress a file, (Compress/Decompress): '))
